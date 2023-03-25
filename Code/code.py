bc78c2188ecbdec245612392a1514bb7b8d9e13d
Carl Clegg <carl@cjc.im>
2023-03-25 20:12:56 +0000

    Updates or something...
    Lots of updates, I am not a professional software dev
    
    * Screen now in use
    * buttons in use
    * Menu to select scrript to run
    * Probably more, who cares?

--- Code/code.py
+++ Code/code.py
@@ -8,53 +8,140 @@ import time
 import board
 import digitalio
 
+import displayio
+import terminalio
+import busio
+from adafruit_display_text import label
+import adafruit_displayio_ssd1306
+
+import os
+
+displayio.release_displays()
+
+i2c = busio.I2C(scl=board.GP27, sda=board.GP26)
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+
+WIDTH = 128
+HEIGHT = 32
+BORDER = 5
+
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=WIDTH, height=HEIGHT)
+
+# Make the display context
+
+
+splash = displayio.Group()
+display.show(splash)
+
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 led.value = False
+
+
+left_button = digitalio.DigitalInOut(board.GP14)
+left_button.switch_to_input(pull=digitalio.Pull.DOWN)
+
+right_button = digitalio.DigitalInOut(board.GP15)
+right_button.switch_to_input(pull=digitalio.Pull.DOWN)
 
 # Set up the keyboard and mouse objects
 keyboard = Keyboard(usb_hid.devices)
 keyboard_layout = KeyboardLayoutUS(keyboard)
 mouse = Mouse(usb_hid.devices)
 
-# Read the list of commands from a text file
-with open('JIGGLE.cy', 'r') as file:
-    commands = file.read().splitlines()
+
 
 # Initialize the program counter to zero
 program_counter = 0
 
-# Iterate through each command in the list
-while program_counter < len(commands):
-    command = commands[program_counter]
-    if command.startswith('TYPE'):
-        # Extract the text to type from the command and type it using the keyboard
-        text = command[5:]
-        keyboard_layout.write(text)
-    elif command.startswith('SLEEP'):
-        # Extract the sleep duration from the command and sleep for that amount of time
-        duration = float(command[6:])
-        time.sleep(duration)
-    elif command == 'RETURN':
-        # Press the return key using the keyboard
-        keyboard.send(Keycode.ENTER)
-    elif command == 'MOD4':
-        # Press the windows key using the keyboard
-        keyboard.press(Keycode.WINDOWS)
-        keyboard.release(Keycode.WINDOWS)
-    elif command.startswith('MOUSE'):
-        # Extract the X and Y coordinates from the command and move the mouse cursor to that position
-        x, y = command[6:].split(',')
-        mouse.move(int(x), int(y))
-    elif command.startswith('JUMP'):
-        # Extract the line number to jump to from the command and update the program counter
-        line_number = int(command[5:])
-        program_counter = line_number
-        continue
-    elif command.startswith('REM'):
-        # Do nothing for a comment
-        pass
-    elif command.startswith("LED"):
-        led.value = not led.value
-    # Increment the program counter
-    program_counter += 1
+# 0 = MENU, 1 = RUNNING
+STATE = 0
+FILENAME = ""
+
+while True:
+    if STATE == 0:
+        files = []
+        for f in os.listdir():
+            if f.endswith('.cy'):
+                files.append(f)
+        index = 0
+        FILENAME = files[index]
+        
+        while STATE == 0:
+            splash = displayio.Group()
+            
+            text_area_top = label.Label(
+                terminalio.FONT, text=FILENAME, color=0xFFFFFF, x=4, y=HEIGHT // 2 - 1
+            )
+            splash.append(text_area_top)
+            
+            display.show(splash)
+            display.refresh()
+            if (right_button.value):
+                STATE = 1
+            elif (left_button.value):
+                index += 1
+                if index >= len(files):
+                    index = 0
+                FILENAME = files[index]
+                    
+            time.sleep(0.1)
+    elif STATE == 1:
+        # Read the list of commands from a text file
+        with open(FILENAME, 'r') as file:
+            commands = file.read().splitlines()
+        top_text = ""
+
+        program_counter = 0
+        while program_counter < len(commands):
+            command = commands[program_counter]
+            splash = displayio.Group()
+            
+            # Make the display context
+            text_area = label.Label(
+                terminalio.FONT, text=str(program_counter) + " " + command, color=0xFFFFFF, x=4, y=HEIGHT-6
+            )
+            splash.append(text_area)
+            
+            text_area_top = label.Label(
+                terminalio.FONT, text=top_text, color=0xFFFFFF, x=4, y=4
+            )
+            splash.append(text_area_top)
+            
+            display.show(splash)
+            display.refresh()
+            if command.startswith('TYPE'):
+                # Extract the text to type from the command and type it using the keyboard
+                text = command[5:]
+                keyboard_layout.write(text)
+            elif command.startswith('SLEEP'):
+                # Extract the sleep duration from the command and sleep for that amount of time
+                duration = float(command[6:])
+                time.sleep(duration)
+            elif command == 'RETURN':
+                # Press the return key using the keyboard
+                keyboard.send(Keycode.ENTER)
+            elif command == 'MOD4':
+                # Press the windows key using the keyboard
+                keyboard.press(Keycode.WINDOWS)
+                keyboard.release(Keycode.WINDOWS)
+            elif command.startswith('MOUSE'):
+                # Extract the X and Y coordinates from the command and move the mouse cursor to that position
+                x, y = command[6:].split(',')
+                mouse.move(int(x), int(y))
+            elif command.startswith('JUMP'):
+                # Extract the line number to jump to from the command and update the program counter
+                line_number = int(command[5:])
+                program_counter = line_number
+                continue
+            elif command.startswith('REM'):
+                # Do nothing for a comment
+                pass
+            elif command.startswith("LED"):
+                led.value = not led.value
+            elif command.startswith("SCREEN"):
+                top_text = command[7:]
+
+            program_counter += 1
+        STATE = 0
+
