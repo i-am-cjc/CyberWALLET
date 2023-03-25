0746f675011de88d504d05f0889bb8425f1bb8a6
Carl Clegg <carl@cjc.im>
2023-03-25 20:10:43 +0000

    Update boot.py
    Disable usb / serial if button is not pressed

--- Code/boot.py
+++ Code/boot.py
@@ -4,12 +4,12 @@ import usb_cdc
 
 # If not pressed, the key will be at +V (due to the pull-up).
 # https://learn.adafruit.com/customizing-usb-devices-in-circuitpython/circuitpy-midi-serial#circuitpy-mass-storage-device-3096583-4
-button = digitalio.DigitalInOut(board.GP2)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
+
+left_button = digitalio.DigitalInOut(board.GP14)
+left_button.switch_to_input(pull=digitalio.Pull.DOWN)
 
 # Disable devices only if button is not pressed.
-if button.value:
+if not left_button.value:
     print(f"boot: button not pressed, disabling drive")
     storage.disable_usb_drive()
     usb_cdc.disable() 
