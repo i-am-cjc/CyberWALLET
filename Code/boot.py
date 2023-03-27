a37d215670f6b128aea569055891e6d247daccd4
Carl Clegg <carl@cjc.im>
2023-03-27 19:48:08 +0100

    Update boot.py
    Meant to be using the correct button

--- Code/boot.py
+++ Code/boot.py
@@ -5,7 +5,7 @@ import usb_cdc
 # If not pressed, the key will be at +V (due to the pull-up).
 # https://learn.adafruit.com/customizing-usb-devices-in-circuitpython/circuitpy-midi-serial#circuitpy-mass-storage-device-3096583-4
 
-left_button = digitalio.DigitalInOut(board.GP14)
+left_button = digitalio.DigitalInOut(board.GP12)
 left_button.switch_to_input(pull=digitalio.Pull.DOWN)
 
 # Disable devices only if button is not pressed.
