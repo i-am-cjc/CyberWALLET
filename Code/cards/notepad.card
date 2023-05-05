58c73c420689c9d351c664340f6726a3b73b8d27
Carl Clegg <carl@cjc.im>
2023-05-05 18:16:16 +0100

    So I went a little mad
    I started to consider the idea of other modules for the cyber wallet, calling them card..(cards, in a wallet, get it?) but realised that I was distracting myself from the main aim of this shit.  So scripts are now cards, and we focus on macro keyboard stuff.

---
+++ Code/cards/notepad.card
@@ -0,0 +1,4 @@
+MOD4 d
+MOD4 r
+RTYPE notepad.exe
+SLEEP 1
--- Code/code.py
+++ Code/code.py
@@ -29,14 +29,12 @@ display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=WIDTH, height=HE
 
 # Make the display context
 
-
 splash = displayio.Group()
 display.show(splash)
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 led.value = False
-
 
 left_button = digitalio.DigitalInOut(board.GP12)
 left_button.switch_to_input(pull=digitalio.Pull.DOWN)
@@ -49,191 +47,193 @@ keyboard = Keyboard(usb_hid.devices)
 keyboard_layout = KeyboardLayoutUS(keyboard)
 mouse = Mouse(usb_hid.devices)
 
+def run():
+    #displayio.release_displays()
 
+    # Initialize the program counter to zero
+    program_counter = 0
 
-# Initialize the program counter to zero
-program_counter = 0
+    # 0 = MENU, 1 = RUNNING
+    STATE = 0
+    FILENAME = ""
 
-# 0 = MENU, 1 = RUNNING
-STATE = 0
-FILENAME = ""
+    DELAY = 0.1
 
-DELAY = 0.1
-
-VARS = {}
-STACK = []
-
-while True:
     VARS = {}
-    if STATE == 0:
-        files = []
-        for f in os.listdir("./scripts"):
-            if f.endswith('.cy'):
-                files.append(f)
-        files.sort()
-        index = 0
-        FILENAME = files[index]
-        
-        while STATE == 0:
-            splash = displayio.Group()
-            
-            text_area_top = label.Label(
-                terminalio.FONT, text=FILENAME[:-3], color=0xFFFFFF, x=4, y=HEIGHT // 2 - 1
-            )
-            splash.append(text_area_top)
-            
-            display.show(splash)
-            display.refresh()
-            if (right_button.value):
-                STATE = 1
-            elif (left_button.value):
-                index += 1
-                if index >= len(files):
-                    index = 0
-                FILENAME = files[index]
-                    
-            time.sleep(0.1)
-    elif STATE == 1:
-        # Read the list of commands from a text file
-        with open("./scripts/" + FILENAME, 'r') as file:
-            commands = file.read().splitlines()
-        top_text = ""
+    STACK = []
 
-        program_counter = 0
-        while program_counter < len(commands):
-            need_to_sleep = False
-            command = commands[program_counter]
-            splash = displayio.Group()
+    while True:
+        VARS = {}
+        if STATE == 0:
+            files = []
+            for f in os.listdir("./cards"):
+                if f.endswith('.card'):
+                    files.append(f)
+            files.sort()
+            index = 0
+            FILENAME = files[index]
             
-            # Make the display context
-            text_area = label.Label(
-                terminalio.FONT, text=str(program_counter) + " " + command, color=0xFFFFFF, x=4, y=HEIGHT-6
-            )
-            splash.append(text_area)
-            
-            text_area_top = label.Label(
-                terminalio.FONT, text=top_text, color=0xFFFFFF, x=4, y=4
-            )
-            splash.append(text_area_top)
-            
-            display.show(splash)
-            display.refresh()
-            if command.startswith('TYPE'):
-                # Extract the text to type from the command and type it using the keyboard
-                text = command[5:]
-                keyboard_layout.write(text)
-                need_to_sleep = True
-            elif command.startswith('SLEEP'):
-                # Extract the sleep duration from the command and sleep for that amount of time
-                duration = float(command[6:])
-                time.sleep(duration)
-            elif command.startswith("DELAY"):
-                DELAY = float(command[6:])
-            elif command.startswith("RTYPE"):
-                text = command[6:]
-                keyboard_layout.write(text)
+            while STATE == 0:
+                splash = displayio.Group()
+                
+                text_area_top = label.Label(
+                    terminalio.FONT, text=FILENAME[:-5], color=0xFFFFFF, x=4, y=HEIGHT // 2 - 1
+                )
+                splash.append(text_area_top)
+                
+                display.show(splash)
+                display.refresh()
+                if (right_button.value):
+                    STATE = 1
+                elif (left_button.value):
+                    index += 1
+                    if index >= len(files):
+                        index = 0
+                    FILENAME = files[index]
+                        
                 time.sleep(0.1)
-                keyboard.send(Keycode.ENTER)
-                need_to_sleep = True
-            elif command == 'RETURN':
-                # Press the return key using the keyboard
-                keyboard.send(Keycode.ENTER)
-                need_to_sleep = True
-            elif command == "TAB":
-                keyboard.send(Keycode.TAB)
-                need_to_sleep = True
-            elif command.startswith('MOD4'):
-                # Press the windows key using the keyboard
-                keyboard.press(Keycode.WINDOWS)
-                if len(command) > 4:
-                    k = command[5:6]
-                    keyboard_layout.write(k)
-                keyboard.release(Keycode.WINDOWS)
-                need_to_sleep = True
-            elif command.startswith('CTRL'):
-                keyboard.press(Keycode.CONTROL)
-                if len(command) > 4:
-                    k = command[5:6]
-                    keyboard_layout.write(k)
-                keyboard.release(Keycode.CONTROL)
-                need_to_sleep = True
-            elif command.startswith('MOUSE'):
-                # Extract the X and Y coordinates from the command and move the mouse cursor to that position
-                x, y = command[6:].split(',')
-                mouse.move(int(x), int(y))
-                need_to_sleep = True
-            elif command.startswith('CLICK'):
-                mouse.click(Mouse.LEFT_BUTTON)
-                need_to_sleep = True
-            elif command.startswith('JUMP'):
-                # Extract the line number to jump to from the command and update the program counter
-                jump_target = command[5:]
-                target = 0
-                for line in commands:
-                    if line == "LABEL " + jump_target:
-                        program_counter = target
-                    target += 1
-                continue
-            elif command.startswith("SET"):
-                var = command[4:]
-                VARS[var] = True
-            elif command.startswith("UNSET"):
-                var = command[6:]
-                if var in VARS:
-                    del VARS[var]
-            elif command.startswith("VJUMP"):
-                var, jump_target = command[6:].split(" ")
-                if var in VARS:
+        elif STATE == 1:
+            # Read the list of commands from a text file
+            with open("./cards/" + FILENAME, 'r') as file:
+                commands = file.read().splitlines()
+            top_text = ""
+
+            program_counter = 0
+            while program_counter < len(commands):
+                need_to_sleep = False
+                command = commands[program_counter]
+                splash = displayio.Group()
+                
+                # Make the display context
+                text_area = label.Label(
+                    terminalio.FONT, text=str(program_counter) + " " + command, color=0xFFFFFF, x=4, y=HEIGHT-6
+                )
+                splash.append(text_area)
+                
+                text_area_top = label.Label(
+                    terminalio.FONT, text=top_text, color=0xFFFFFF, x=4, y=4
+                )
+                splash.append(text_area_top)
+                
+                display.show(splash)
+                display.refresh()
+                if command.startswith('TYPE'):
+                    # Extract the text to type from the command and type it using the keyboard
+                    text = command[5:]
+                    keyboard_layout.write(text)
+                    need_to_sleep = True
+                elif command.startswith('SLEEP'):
+                    # Extract the sleep duration from the command and sleep for that amount of time
+                    duration = float(command[6:])
+                    time.sleep(duration)
+                elif command.startswith("DELAY"):
+                    DELAY = float(command[6:])
+                elif command.startswith("RTYPE"):
+                    text = command[6:]
+                    keyboard_layout.write(text)
+                    time.sleep(0.1)
+                    keyboard.send(Keycode.ENTER)
+                    need_to_sleep = True
+                elif command == 'RETURN':
+                    # Press the return key using the keyboard
+                    keyboard.send(Keycode.ENTER)
+                    need_to_sleep = True
+                elif command == "TAB":
+                    keyboard.send(Keycode.TAB)
+                    need_to_sleep = True
+                elif command.startswith('MOD4'):
+                    # Press the windows key using the keyboard
+                    keyboard.press(Keycode.WINDOWS)
+                    if len(command) > 4:
+                        k = command[5:6]
+                        keyboard_layout.write(k)
+                    keyboard.release(Keycode.WINDOWS)
+                    need_to_sleep = True
+                elif command.startswith('CTRL'):
+                    keyboard.press(Keycode.CONTROL)
+                    if len(command) > 4:
+                        k = command[5:6]
+                        keyboard_layout.write(k)
+                    keyboard.release(Keycode.CONTROL)
+                    need_to_sleep = True
+                elif command.startswith('MOUSE'):
+                    # Extract the X and Y coordinates from the command and move the mouse cursor to that position
+                    x, y = command[6:].split(',')
+                    mouse.move(int(x), int(y))
+                    need_to_sleep = True
+                elif command.startswith('CLICK'):
+                    mouse.click(Mouse.LEFT_BUTTON)
+                    need_to_sleep = True
+                elif command.startswith('JUMP'):
+                    # Extract the line number to jump to from the command and update the program counter
+                    jump_target = command[5:]
                     target = 0
                     for line in commands:
                         if line == "LABEL " + jump_target:
                             program_counter = target
                         target += 1
-                else:
-                    program_counter += 1
-                continue
-            elif command.startswith("BJUMP"):
-                # Jump if left button is pressed, usually proceeded with a SCREEN command
-                l = True
-                jump_target =command[6:]
-                while l == True:
-                    if left_button.value:
+                    continue
+                elif command.startswith("SET"):
+                    var = command[4:]
+                    VARS[var] = True
+                elif command.startswith("UNSET"):
+                    var = command[6:]
+                    if var in VARS:
+                        del VARS[var]
+                elif command.startswith("VJUMP"):
+                    var, jump_target = command[6:].split(" ")
+                    if var in VARS:
                         target = 0
                         for line in commands:
                             if line == "LABEL " + jump_target:
                                 program_counter = target
                             target += 1
-                        l = False
-                        continue
-                    elif right_button.value:
+                    else:
                         program_counter += 1
-                        l = False
-                    time.sleep(0.1)
-                continue
-            elif command.startswith('REM'):
-                # Do nothing for a comment
-                pass
-            elif command.startswith("LED"):
-                led.value = not led.value
-            elif command.startswith("SCREEN"):
-                top_text = command[7:]
-            elif command.startswith("INC"):
-                f = command[4:]
-                # PC + 1 as it would otherwise include the file again, lol
-                STACK.append( (commands, program_counter + 1) )
-                program_counter = 0
-                with open("./scripts/" + f + ".cy", 'r') as file:
-                    commands = file.read().splitlines()
-                continue
+                    continue
+                elif command.startswith("BJUMP"):
+                    # Jump if left button is pressed, usually proceeded with a SCREEN command
+                    l = True
+                    jump_target =command[6:]
+                    while l == True:
+                        if left_button.value:
+                            target = 0
+                            for line in commands:
+                                if line == "LABEL " + jump_target:
+                                    program_counter = target
+                                target += 1
+                            l = False
+                            continue
+                        elif right_button.value:
+                            program_counter += 1
+                            l = False
+                        time.sleep(0.1)
+                    continue
+                elif command.startswith('REM'):
+                    # Do nothing for a comment
+                    pass
+                elif command.startswith("LED"):
+                    led.value = not led.value
+                elif command.startswith("SCREEN"):
+                    top_text = command[7:]
+                elif command.startswith("INC"):
+                    f = command[4:]
+                    # PC + 1 as it would otherwise include the file again, lol
+                    STACK.append( (commands, program_counter + 1) )
+                    program_counter = 0
+                    with open("./cards/" + f + ".card", 'r') as file:
+                        commands = file.read().splitlines()
+                    continue
 
-            program_counter += 1
+                program_counter += 1
 
 
-            if program_counter >= len(commands) and len(STACK) > 0:
-                commands, program_counter = STACK.pop()
+                if program_counter >= len(commands) and len(STACK) > 0:
+                    commands, program_counter = STACK.pop()
 
 
-            if need_to_sleep:
-                time.sleep(DELAY)
-        STATE = 0
+                if need_to_sleep:
+                    time.sleep(DELAY)
+            STATE = 0
 
+run()
